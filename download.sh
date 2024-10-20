09085dce62fca4a43ca3767e38b39ec5a0af14ee
Jon Moss <maclover7@users.noreply.github.com>
2024-10-19 22:48:44 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-url="https://www.dos.pa.gov/VotingElections/OtherServicesEvents/VotingElectionStatistics/Documents/currentvotestats.xls"
+url="https://www.pa.gov/content/dam/copapwp-pagov/en/dos/resources/voting-and-elections/voting-and-election-statistics/currentvotestats.xls"
 name="stats/$(date '+%Y-%m-%d').xls"
 
 curl $url --output $name
